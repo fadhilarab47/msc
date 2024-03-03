f2f2c6a5fc15a60185410417d6f557cb49bf37d9
Tirox XF <161807690+cioxio@users.noreply.github.com>
2024-03-03 19:58:27 +0530

    Create logger.py

---
+++ BgtxD/modules/sudo/logger.py
@@ -0,0 +1,31 @@
+
+
+from pyrogram import filters
+
+from BgtxD import config
+from BgtxD.power import get_command
+from BgtxD import app
+from BgtxD.misc import SUDOERS
+from BgtxD.utility.database import add_off, add_on
+from BgtxD.utility.decorators.language import language
+
+# Commands
+LOGGER_COMMAND = get_command("LOGGER_COMMAND")
+
+
+@app.on_message(filters.command(LOGGER_COMMAND) & SUDOERS)
+@language
+async def logger(client, message, _):
+    usage = _["log_1"]
+    if len(message.command) != 2:
+        return await message.reply_text(usage)
+    state = message.text.split(None, 1)[1].strip()
+    state = state.lower()
+    if state == "enable":
+        await add_on(config.LOG)
+        await message.reply_text(_["log_2"])
+    elif state == "disable":
+        await add_off(config.LOG)
+        await message.reply_text(_["log_3"])
+    else:
+        await message.reply_text(usage)
