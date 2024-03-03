90e71241f3e23fdc1698c9fce130dd2ddf1c4c24
cioxio <161807690+cioxio@users.noreply.github.com>
2024-03-03 15:37:12 +0530

    Create __init__.py

---
+++ BgtxD/__init__.py
@@ -0,0 +1,40 @@
+# BGT-MUSIC
+
+from BgtxD.core.bot import BGT
+from BgtxD.core.dir import dirr
+from BgtxD.core.git import git
+from BgtxD.core.userbot import Userbot
+from BgtxD.misc import dbb, heroku, sudo
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = BGT()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
