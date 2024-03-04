f0837992b5af4b944a3a2e566448f871aabd0313
Tirox XF <161807690+cioxio@users.noreply.github.com>
2024-03-04 17:35:13 +0530

    Update bot.py

--- BgtxD/centre/bot.py
+++ BgtxD/centre/bot.py
@@ -8,7 +8,7 @@ from pyrogram.types import BotCommand
 from pyrogram.enums import ChatMemberStatus
 
 
-class BIKASH(Client):
+class BGT(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
