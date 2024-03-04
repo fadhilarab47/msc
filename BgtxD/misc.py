6d99f20e925c98bdae8491aede511990ede38e1d
Tirox XF <161807690+cioxio@users.noreply.github.com>
2024-03-05 01:46:39 +0530

    Update misc.py

--- BgtxD/misc.py
+++ BgtxD/misc.py
@@ -42,25 +42,6 @@ def dbb():
     LOGGER(__name__).info(f"Local Database Initialized.")
 
 
-async def sudo():
-    global SUDOERS
-    SUDOERS.add(config.OWNER_ID)
-    sudoersdb = mongodb.sudoers
-    sudoers = await sudoersdb.find_one({"sudo": "sudo"})
-    sudoers = [] if not sudoers else sudoers["sudoers"]
-    if config.OWNER_ID not in sudoers:
-        sudoers.append(config.OWNER_ID)
-        await sudoersdb.update_one(
-            {"sudo": "sudo"},
-            {"$set": {"sudoers": sudoers}},
-            upsert=True,
-        )
-    if sudoers:
-        for user_id in sudoers:
-            SUDOERS.add(user_id)
-    LOGGER(__name__).info(f"Sudoers Loaded.")
-
-
 def heroku():
     global HAPP
     if is_heroku:
