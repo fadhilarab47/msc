f59c5f51bf42b02461654f06748a1d8c1712bdc4
Tirox XF <161807690+cioxio@users.noreply.github.com>
2024-03-05 00:08:13 +0530

    Update stats.py

--- BgtxD/modules/tools/stats.py
+++ BgtxD/modules/tools/stats.py
@@ -17,7 +17,7 @@ from BgtxD.config import BANNED_USERS, MUSIC_BOT_NAME
 from BgtxD.power import get_command
 from BgtxD import YouTube, app
 from BgtxD.centre.userbot import assistants
-from BgtxD.misc import SUDOERS, pymongodb
+from BgtxD.misc import SUDOERS, mongodb
 from BgtxD.modules import ALL_MODULES
 from BgtxD.utility.database import (get_global_tops,
                                        get_particulars, get_queries,
