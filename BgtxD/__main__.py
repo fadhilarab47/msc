62f26472282b1ee46a6109181b00307e9b8f1b81
Tirox XF <161807690+cioxio@users.noreply.github.com>
2024-03-04 23:51:03 +0530

    Update __main__.py

--- BgtxD/__main__.py
+++ BgtxD/__main__.py
@@ -51,17 +51,6 @@ async def init():
     )
     await userbot.start()
     await BIKASH.start()
-    try:
-        await BIKASH.stream_call(
-            "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
-        )
-    except NoActiveGroupCall:
-        LOGGER("BgtxD").error(
-            "[ERROR] - \n\nPlease turn on your Logger Group's Voice Call. Make sure you never close/end voice call in your log group"
-        )
-        sys.exit()
-    except:
-        pass
     await BIKASH.decorators()
     LOGGER("BgtxD").info("BGT Music Bot Started Successfully")
     await idle()
