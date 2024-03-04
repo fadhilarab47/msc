c8ef02ab3f23fef2fcdc000f5609b7fe2a032bd9
Tirox XF <161807690+cioxio@users.noreply.github.com>
2024-03-05 00:55:30 +0530

    Update userbot.py

--- BgtxD/centre/userbot.py
+++ BgtxD/centre/userbot.py
@@ -81,15 +81,7 @@ class Userbot(Client):
             except:
                 pass
             assistants.append(2)
-            try:
-                await self.two.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
-                )
-            except:
-                LOGGER(__name__).error(
-                    f"Assistant Account 2 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
-                )
-                sys.exit()
+            
             get_me = await self.two.get_me()
             self.two.username = get_me.username
             self.two.id = get_me.id
@@ -111,15 +103,7 @@ class Userbot(Client):
             except:
                 pass
             assistants.append(3)
-            try:
-                await self.three.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
-                )
-            except:
-                LOGGER(__name__).error(
-                    f"Assistant Account 3 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
-                )
-                sys.exit()
+            
             get_me = await self.three.get_me()
             self.three.username = get_me.username
             self.three.id = get_me.id
@@ -141,15 +125,7 @@ class Userbot(Client):
             except:
                 pass
             assistants.append(4)
-            try:
-                await self.four.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
-                )
-            except:
-                LOGGER(__name__).error(
-                    f"Assistant Account 4 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
-                )
-                sys.exit()
+            
             get_me = await self.four.get_me()
             self.four.username = get_me.username
             self.four.id = get_me.id
@@ -171,15 +147,7 @@ class Userbot(Client):
             except:
                 pass
             assistants.append(5)
-            try:
-                await self.five.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
-                )
-            except:
-                LOGGER(__name__).error(
-                    f"Assistant Account 5 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
-                )
-                sys.exit()
+            
             get_me = await self.five.get_me()
             self.five.username = get_me.username
             self.five.id = get_me.id
