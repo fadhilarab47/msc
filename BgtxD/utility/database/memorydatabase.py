7ce4d5ff2c4b9edd676e222a5448ac7844ec1a2e
Tirox XF <161807690+cioxio@users.noreply.github.com>
2024-03-05 19:04:43 +0530

    Update memorydatabase.py

--- BgtxD/utility/database/memorydatabase.py
+++ BgtxD/utility/database/memorydatabase.py
@@ -451,13 +451,10 @@ async def maintenance_on():
 
 # Audio Video Limit
 
-from pytgcalls.types.input_stream.quality import (HighQualityAudio,
-                                                  HighQualityVideo,
-                                                  LowQualityAudio,
-                                                  LowQualityVideo,
-                                                  MediumQualityAudio,
-                                                  MediumQualityVideo)
-
+from pytgcalls.types import (
+    AudioQuality,
+    VideoQuality,
+)
 
 async def save_audio_bitrate(chat_id: int, bitrate: str):
     audio[chat_id] = bitrate
@@ -487,25 +484,25 @@ async def get_vid_bit_name(chat_id: int) -> str:
 async def get_audio_bitrate(chat_id: int) -> str:
     mode = audio.get(chat_id)
     if not mode:
-        return MediumQualityAudio()
+        return AudioQuality.STUDIO
     if str(mode) == "High":
-        return HighQualityAudio()
+        return AudioQuality.STUDIO
     elif str(mode) == "Medium":
-        return MediumQualityAudio()
+        return AudioQuality.STUDIO
     elif str(mode) == "Low":
-        return LowQualityAudio()
+        return AudioQuality.STUDIO
 
 
 async def get_video_bitrate(chat_id: int) -> str:
     mode = video.get(chat_id)
     if not mode:
         if PRIVATE_BOT_MODE == str(True):
-            return HighQualityVideo()
+            return VideoQuality.SD_360p
         else:
-            return MediumQualityVideo()
+            return VideoQuality.SD_360p
     if str(mode) == "High":
-        return HighQualityVideo()
+        return VideoQuality.SD_360p
     elif str(mode) == "Medium":
-        return MediumQualityVideo()
+        return VideoQuality.SD_360p
     elif str(mode) == "Low":
-        return LowQualityVideo()
+        return VideoQuality.SD_360p
