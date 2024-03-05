41163f59b8f5a12fa469eaa5848f2887f260d78a
Tirox XF <161807690+cioxio@users.noreply.github.com>
2024-03-05 16:33:55 +0530

    Update __init__.py

--- BgtxD/__init__.py
+++ BgtxD/__init__.py
@@ -2,16 +2,29 @@ from BgtxD.centre.bot import BGT
 from BgtxD.centre.dir import dirr
 from BgtxD.centre.git import git
 from BgtxD.centre.userbot import Userbot
-from BgtxD.misc import dbb, heroku
+from BgtxD.misc import dbb, heroku, sudo
 
 from .logging import LOGGER
 
+# Directories
 dirr()
+
+# Check Git Updates
 git()
+
+# Initialize Memory DB
 dbb()
+
+# Heroku APP
 heroku()
 
+# Load Sudo Users from DB
+sudo()
+
+### ----------
 app = BGT()
+
+### ----------
 userbot = Userbot()
 
 
