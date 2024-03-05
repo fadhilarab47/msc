c4be1868f44da4ba10f8eef9715451dd98317ea6
Tirox XF <161807690+cioxio@users.noreply.github.com>
2024-03-05 12:33:05 +0530

    Update misc.py

--- BgtxD/misc.py
+++ BgtxD/misc.py
@@ -1,3 +1,4 @@
+
 import socket
 import time
 
@@ -5,7 +6,7 @@ import heroku3
 from pyrogram import filters
 
 from BgtxD import config
-from BgtxD.centre.mongo import mongodb
+from BgtxD.centre.mongo import pymongodb
 
 from .logging import LOGGER
 
@@ -32,14 +33,39 @@ XCB = [
     "https",
     str(config.HEROKU_APP_NAME),
     "HEAD",
-    "master",
+    "main",
 ]
 
 
 def dbb():
     global db
     db = {}
-    LOGGER(__name__).info(f"Local Database Initialized.")
+    LOGGER(__name__).info(f"Database Initialized.")
+
+
+def sudo():
+    global SUDOERS
+    OWNER = config.OWNER_ID
+    if config.MONGO_DB_URI is None:
+        for user_id in OWNER:
+            SUDOERS.add(user_id)
+    else:
+        sudoersdb = mongodb.sudoers
+        sudoers = sudoersdb.find_one({"sudo": "sudo"})
+        sudoers = [] if not sudoers else sudoers["sudoers"]
+        for user_id in OWNER:
+            SUDOERS.add(user_id)
+            if user_id not in sudoers:
+                sudoers.append(user_id)
+                sudoersdb.update_one(
+                    {"sudo": "sudo"},
+                    {"$set": {"sudoers": sudoers}},
+                    upsert=True,
+                )
+        if sudoers:
+            for x in sudoers:
+                SUDOERS.add(x)
+    LOGGER(__name__).info(f"Sudoers Loaded.")
 
 
 def heroku():
