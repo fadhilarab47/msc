4191e03efe259e1e8b40576e22536f2418c4bbcf
Tirox XF <161807690+cioxio@users.noreply.github.com>
2024-03-05 20:55:58 +0530

    Update thumbnails.py

--- BgtxD/utility/thumbnails.py
+++ BgtxD/utility/thumbnails.py
@@ -75,6 +75,7 @@ async def gen_thumb(videoid):
         background = enhancer.enhance(0.6)
         y = circle(zyoutube).resize((474, 474))
         background.paste(y, (50, 100), mask=y)  # Adjusted placement of YouTube circle image
+        logo.thumbnail((520, 520), Image.ANTIALIAS)
         logo = ImageOps.expand(logo, border=15, fill="white")
         background.paste(logo, (50, 100))
         draw = ImageDraw.Draw(background)
