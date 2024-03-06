cb5947cbe915ca56ab02be448c7b7df8bdfc579d
Tirox XF <161807690+cioxio@users.noreply.github.com>
2024-03-06 18:37:40 +0530

    Update thumbnails.py

--- BgtxD/utility/thumbnails.py
+++ BgtxD/utility/thumbnails.py
@@ -84,7 +84,7 @@ async def gen_thumb(videoid):
         logo = circle(youtube).resize((474, 474))
         background.paste(logo, (50, 100), mask=logo)  # Adjusted placement of YouTube circle image
         draw = ImageDraw.Draw(background)
-        draw.pieslice([(0,0), (720, 720)], 0, 360, fill = 15, outline = "blue")
+      # draw.pieslice([(0,0), (720, 720)], 0, 360, fill = 15, outline = "blue")
         font = ImageFont.truetype("BgtxD/power/font2.ttf", 40)
         font2 = ImageFont.truetype("BgtxD/power/font2.ttf", 70)
         arial = ImageFont.truetype("BgtxD/power/font2.ttf", 30)
