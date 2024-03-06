70bad3eb56b606748a2f1ea5a3c733c1dfd41fc3
Tirox XF <161807690+cioxio@users.noreply.github.com>
2024-03-06 13:13:21 +0530

    Update thumbnails.py

--- BgtxD/utility/thumbnails.py
+++ BgtxD/utility/thumbnails.py
@@ -67,13 +67,13 @@ async def gen_thumb(videoid):
                     await f.close()
 
         youtube = Image.open(f"cache/thumb{videoid}.png")
-        zyoutube = Image.open(f"cache/thumb{videoid}.png")
         image1 = changeImageSize(1280, 720, youtube)
         image2 = image1.convert("RGBA")
         background = image2.filter(filter=ImageFilter.BoxBlur(30))
         enhancer = ImageEnhance.Brightness(background)
         background = enhancer.enhance(0.6)
-        y = circle(zyoutube).resize((474, 474))
+        y = circle(youtube).resize((474, 474))
+        logo = ImageOps.expand(logo, border=15, fill="white")
         background.paste(y, (50, 100), mask=y)  # Adjusted placement of YouTube circle image
         draw = ImageDraw.Draw(background)
         font = ImageFont.truetype("BgtxD/power/font2.ttf", 40)
