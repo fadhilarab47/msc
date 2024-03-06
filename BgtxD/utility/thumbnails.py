85e266f46174e606a9527bb0656010b697f440db
Tirox XF <161807690+cioxio@users.noreply.github.com>
2024-03-06 12:22:42 +0530

    Update thumbnails.py

--- BgtxD/utility/thumbnails.py
+++ BgtxD/utility/thumbnails.py
@@ -1,8 +1,7 @@
-
 import os
 import re
 import textwrap
-
+import numpy as np
 import aiofiles
 import aiohttp
 from PIL import (Image, ImageDraw, ImageEnhance, ImageFilter,
@@ -20,6 +19,16 @@ def changeImageSize(maxWidth, maxHeight, image):
     newImage = image.resize((newWidth, newHeight))
     return newImage
 
+
+def circle(img):
+    h, w = img.size
+    a = Image.new('L', [h, w], 0)
+    b = ImageDraw.Draw(a)
+    b.pieslice([(0, 0), (h, w)], 0, 360, fill=255, outline="white")
+    c = np.array(img)
+    d = np.array(a)
+    e = np.dstack((c, d))
+    return Image.fromarray(e)
 
 async def gen_thumb(videoid):
     if os.path.isfile(f"cache/{videoid}.png"):
@@ -58,21 +67,72 @@ async def gen_thumb(videoid):
                     await f.close()
 
         youtube = Image.open(f"cache/thumb{videoid}.png")
+        zyoutube = Image.open(f"cache/thumb{videoid}.png")
         image1 = changeImageSize(1280, 720, youtube)
         image2 = image1.convert("RGBA")
         background = image2.filter(filter=ImageFilter.BoxBlur(30))
         enhancer = ImageEnhance.Brightness(background)
-        background = enhancer.enhance(1.0)
-        
-        logo.thumbnail((900, 900))
-        logo = ImageOps.expand(logo, border=15, fill="blue")
-        background.paste(logo, (100, 150))
+        background = enhancer.enhance(0.6)
+        y = circle(zyoutube).resize((474, 474))
+        background.paste(y, (50, 100), mask=y)  # Adjusted placement of YouTube circle image
         draw = ImageDraw.Draw(background)
-        font = ImageFont.truetype("BgtxD/power/font2.ttf", 1)
-        font2 = ImageFont.truetype("BgtxD/power/font2.ttf", 1)
-        arial = ImageFont.truetype("BgtxD/power/font2.ttf", 1)
-        name_font = ImageFont.truetype("BgtxD/power/font.ttf", 1)
-        
+        font = ImageFont.truetype("BgtxD/power/font2.ttf", 40)
+        font2 = ImageFont.truetype("BgtxD/power/font2.ttf", 70)
+        arial = ImageFont.truetype("BgtxD/power/font2.ttf", 30)
+        name_font = ImageFont.truetype("BgtxD/power/font.ttf", 30)
+        para = textwrap.wrap(title, width=32)
+        j = 0
+        draw.text(
+            (5, 5), f"{MUSIC_BOT_NAME}", fill="white", font=name_font
+        )
+        draw.text(
+            (600, 150),
+            "NOW PLAYING",
+            fill="white",
+            stroke_width=2,
+            stroke_fill="white",
+            font=font2,
+        )
+        for line in para:
+            if j == 1:
+                j += 1
+                draw.text(
+                    (600, 340),
+                    f"{line}",
+                    fill="white",
+                    stroke_width=1,
+                    stroke_fill="white",
+                    font=font,
+                )
+            if j == 0:
+                j += 1
+                draw.text(
+                    (600, 280),
+                    f"{line}",
+                    fill="white",
+                    stroke_width=1,
+                    stroke_fill="white",
+                    font=font,
+                )
+
+        draw.text(
+            (600, 450),
+            f"Views : {views[:23]}",
+            (255, 255, 255),
+            font=arial,
+        )
+        draw.text(
+            (600, 500),
+            f"Duration : {duration[:23]} Mins",
+            (255, 255, 255),
+            font=arial,
+        )
+        draw.text(
+            (600, 550),
+            f"Channel : {channel}",
+            (255, 255, 255),
+            font=arial,
+        )
         try:
             os.remove(f"cache/thumb{videoid}.png")
         except:
